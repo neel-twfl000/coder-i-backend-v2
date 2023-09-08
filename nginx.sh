81dc3e9399e2dc96208310e49cbb195ca68bd0b9
indranil.swarnakar <indranil.swarnakar@gmail.com>
2023-09-08 16:14:27 +0530

    d

--- nginx.sh
+++ nginx.sh
@@ -1,8 +1,4 @@
-# sudo cp -rf testnginx /etc/nginx/sites-available
-# chmod 710 /var/lib/jenkins/workspace/coder-i-backend
-# sudo ln -s /etc/nginx/sites-available/testnginx /etc/nginx/sites-enabled
 sudo nginx -t
-sudo certbot renew --dry-run
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
 sudo systemctl status nginx
