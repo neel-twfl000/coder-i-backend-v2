0fe3da414203c4cf3261b66bdf9d750ddf8200b7
indranil.swarnakar <indranil.swarnakar@gmail.com>
2023-09-08 16:26:25 +0530

    d

--- gunicorn.sh
+++ gunicorn.sh
@@ -4,15 +4,13 @@ echo "Requeriments Install By Pip Start"
 pip install -r app/requirements.txt
 echo "Requeriments Install By Pip Success"
 echo "Pip Complete"
-
 python3 app/manage.py makemigrations
 python3 app/manage.py migrate
-echo "Migrations done"
 sudo cp -rf gunicorn.socket /etc/systemd/system/
 sudo cp -rf gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable gunicorn
 sudo systemctl start gunicorn
 sudo systemctl status gunicorn
-source env/bin/activate
-echo "###############Gunicorn Setup Done ############"
+# source env/bin/activate
+echo "############### Gunicorn Setup Done ############"
